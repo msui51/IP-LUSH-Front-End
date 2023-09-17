338b7c849950d8cd9405881fb1ab6e3496564f56
Maria Sui <maria.sui51@gmail.com>
2023-09-17 19:24:44 -0400

    added basic structure still needs fixing

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Nav from '../../Components/Nav/Nav';
+import Footer from '../../Components/Footer/Footer';
 import './homePage.scss';
+import { useEffect, useState } from 'react';
 
 function HomePage() {
+  const [isHomePage, setIsHomePage]=useState(false);
+  useEffect(()=>{
+    setIsHomePage(true);
+  },[]);
+ 
+
   return (
-    <div className='homePage'>
+    <>
+      <div className='homePage'>
         <Nav/>
-    </div>
+      </div>
+      <Footer isHomePage={isHomePage} setIsHomePage={setIsHomePage} />
+    </>
   )
 }
 
