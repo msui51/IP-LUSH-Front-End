87a3dbdbe147f4d2c42fdc321f6ad87ffccf9b54
Maria Sui <maria.sui51@gmail.com>
2023-09-18 17:21:23 -0400

    added structure

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import Nav from '../../Components/Nav/Nav';
 import Footer from '../../Components/Footer/Footer';
 import './homePage.scss';
 import { useEffect, useState } from 'react';
+import ItemList from '../../Components/ItemList/ItemList';
 
 function HomePage() {
   const [isHomePage, setIsHomePage]=useState(false);
@@ -15,6 +16,7 @@ function HomePage() {
     <>
       <div className='homePage'>
         <Nav/>
+        <ItemList/>
       </div>
       <Footer isHomePage={isHomePage} setIsHomePage={setIsHomePage} />
     </>
