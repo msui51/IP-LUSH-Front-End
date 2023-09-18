e307a4b9ef3dee15039d4894064646705c243379
roganc11 <125153930+roganc11@users.noreply.github.com>
2023-09-18 16:34:50 -0700

    got images on item card rendering

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './item.scss';
 
-function Item({name, product, price, weight, image}) {
+function Item({ name, product, price, weight, image }) {
   return (
     <div className='item'>
-      <img className='item__image' src={image} alt='soap bar'></img>
+      <img className='item__image' src={require('../../Assets/Images/Frame 14.png')} alt='soap bar'></img>
       <p className='item__title'>{name}</p>
       <div className='item__bottom-wrapper'>  
         <p className='item__price-weight'>{price}/{weight}</p>
         <svg className='item__add-icon' width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path id="Icon" d="M10.7501 4.35004L10.7501 15.15M17.0501 9.75004L4.45007 9.75004" stroke="black" stroke-width="2.25" stroke-linecap="round"/>
+          <path id="Icon" d="M10.7501 4.35004L10.7501 15.15M17.0501 9.75004L4.45007 9.75004" stroke="black" strokeWidth="2.25" strokeLinecap="round" />
         </svg>
       </div>
     </div>
-  )
+  );
 }
 
-export default Item
+export default Item;
--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -5,26 +5,26 @@ import data from '../../Assets/Data/test.json';
 import { ScrollArea } from '@mantine/core';
 
 function ItemList() {
+  // Log data.image here
+
   return (
     <div className='itemList'>
-      <ScrollArea >
-          <ul className='itemList__list'>
-            {data.map((data)=>(
-              <li className='itemList__item'>
-                <Item 
-                  name={data.name}
-                  product={data.product}
-                  price={data.price}
-                  weight={data.weight}
-                  image={data.image}
-                  />
-              </li>
-            ))}
-          </ul>
+      <ScrollArea>
+        <ul className='itemList__list'>
+          {data.map((data) => (
+            <li className='itemList__item' key={data.id}>
+              <Item
+                name={data.name}
+                product={data.product}
+                price={data.price}
+                weight={data.weight}
+              />
+            </li>
+          ))}
+        </ul>
       </ScrollArea>
-      
     </div>
-  )
+  );
 }
 
-export default ItemList
+export default ItemList;
